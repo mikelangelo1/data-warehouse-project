c5d13883391d6d85e073adfe6f1ac52ae5d12f45
mikelangelo1 <michael.oluwasola@stu.cu.edu.ng>
2024-11-12 00:33:19 +0800

    Fixed

--- data/myshop_data.sql
+++ data/myshop_data.sql
@@ -1,19 +1,3 @@
-#
-# Converted from MS Access 2010 myshop database (myshop.accdb) using
-# Bullzip MS Access to MySQL Version 5.1.242. http://www.bullzip.com
-#
-# CHANGES MADE AFTER INITIAL CONVERSION
-# * column and row names in CamelCase converted to lower_case_with_underscore
-# * space and slash ("/") in table and column names replaced with _underscore_
-# * id column names converted to "id"
-# * foreign key column names converted to xxx_id
-# * variables of type TIMESTAMP converted to DATETIME to avoid TIMESTAMP
-#   range limitation (1997 - 2038 UTC), and other limitations.
-# * unique and foreign key checks disabled while loading data
-#
-#------------------------------------------------------------------
-#
-
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 
